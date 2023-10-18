76d56c714c425517e09ea0e8beac512746539ec1
Ryan Pratama <ru.ryanpratama@gmail.com>
2023-10-18 17:47:38 +0700

    adjusted

--- src/app/users/components/Tokyo.tsx
+++ src/app/users/components/Tokyo.tsx
@@ -4,6 +4,9 @@ import { getTokyoTime } from "@/api/users/route";
 import { useQuery } from "@tanstack/react-query";
 
 export default function Tokyo() {
-  const { data: time } = useQuery<any>(["tokyo"], () => getTokyoTime());
+  const { data: time } = useQuery<any>({
+    queryKey: ["tokyo"],
+    queryFn: () => getTokyoTime(),
+  });
   return <h5>Tokyo time: {time?.datetime}</h5>;
 }
